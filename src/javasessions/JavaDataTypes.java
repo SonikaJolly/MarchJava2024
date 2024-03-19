357ff82af3b7346800eab6545ab694a82e455a65
unknown <sonikajolly1996@gmail.com>
2024-03-19 18:39:15 +0530

    javadatatypes

---
+++ src/javasessions/JavaDataTypes.java
@@ -0,0 +1,89 @@
+package javasessions;
+
+public class JavaDataTypes {
+
+	public static void main(String[] args) {
+	
+		byte a = 20;
+		byte b = 120;
+		//byte c; // why byte c is not taking c= a+b ?
+
+		System.out.println(a+b);
+		
+		int x= 200;
+		int y = 100;
+		//int z = x+y;
+	
+		System.out.println((x+y));
+		
+		// if i want to write some message say "the value of Z is (x+y) then how should we quote it in print statement
+		
+		float s= 10.08f;
+		float j= 5.08f;
+		float m = s-j;
+	
+		System.out.println(m);
+		
+		long t= 10000000000l;
+		long r= 500000090909090l;
+		
+		System.out.println(t+r);
+		
+		double d = 23.9807876543298765409;
+		double e= 89.6789098;
+		System.out.println(d*e);
+		
+		float p= 1.2f;
+		System.out.println(p);
+		
+		
+		//a-z: 97 to 122
+		//A-Z: 65 to 90      
+		//0-9: 48 to 57
+		
+		char k = 'A';
+		char l= 'B';
+		char n= '7';
+		
+		
+		System.out.println(k+l);
+		System.out.println(j);
+		
+		System.out.println(k);
+		System.out.println(l);
+		System.out.println(n*l);
+		System.out.println(n);
+		
+		boolean flag = true;
+		boolean flag1=false;
+		
+		System.out.println(flag1);
+		
+		int order1= 38000;
+		int order2 = 8900;
+		int order3= 76543;
+		float order4= order3/order2;
+		
+		
+		System.out.println((order1+order2)+ order3);
+		System.out.println(order1*order2);
+		System.out.println(order4);
+		System.out.println(order1-order3);
+		
+		
+		short v= 7643;
+		short u= 0;
+		
+		System.out.println(v);
+		
+		
+			
+				
+				
+
+		
+		
+
+	}
+
+}
